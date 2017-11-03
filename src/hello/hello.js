028a64d1858516c6cafb32227067314bf46c96ac
ThomasChavatte <thomas.chavatte@etu.unilim.fr>
2017-11-03 21:53:45 +0100

    Update hello.js

--- src/hello/hello.js
+++ src/hello/hello.js
@@ -1,5 +1,10 @@
 // Hi, there
 // use console.log to print something
 module.exports = () => {
- console.log("tt");
+ ?[What is the answer to Life, the Universe and Everything?]
+-[ ] There is no answer to that!
+-[ ] Sleep and eat
+-[x] Easy, this is 42
+-[ ] Peace & Love
+
 };
